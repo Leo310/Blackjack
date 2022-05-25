23a6f41c6c6e99c9614362d3549581f298af75d8
Sebastian Paul <sebastian.paul01@sap.com>
2022-05-25 16:32:24 +0200

    Add: Blackjack and Card: attributes, methods

--- src/main/java/hwr/oop/Blackjack.java
+++ src/main/java/hwr/oop/Blackjack.java
@@ -1,4 +1,16 @@
 package hwr.oop;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Blackjack {
+    Collection<Player> players = new ArrayList<>();
+    Dealer dealer;
+    final int upperBetLimit;
+
+    Blackjack (int upperBetLimit) {
+        this.upperBetLimit = upperBetLimit;
+    }
+
+    void play(){}
 }
--- src/main/java/hwr/oop/Card.java
+++ src/main/java/hwr/oop/Card.java
@@ -1,4 +1,14 @@
 package hwr.oop;
 
-public class Card {
+class Card {
+    private final String name;
+    private final int value;
+
+    Card (String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    int getValue() { return value; }
+    String getName() { return name; }
 }
