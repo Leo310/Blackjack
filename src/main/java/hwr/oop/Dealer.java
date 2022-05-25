e38166a37108dadbc756b1965c782e966986c246
Leo310 <leodev310@gmail.com>
2022-05-25 16:54:49 +0200

    Add: basic player and dealer class

--- src/main/java/hwr/oop/Dealer.java
+++ src/main/java/hwr/oop/Dealer.java
@@ -1,4 +1,29 @@
 package hwr.oop;
 
 public class Dealer {
+    private List deck;
+    private List hand;
+
+    public Dealer() {
+        this.deck = new ArrayList<Card>();
+        // Cards from 2 to 10
+        for (int i = 2; i < 11; i++)
+            this.deck.add(new Card(i, Integer.toString(i)));
+
+        for (int i = 0; i < 4; i++) {
+            this.deck.add(new Card(10, "Jack"));
+            this.deck.add(new Card(10, "Queen"));
+            this.deck.add(new Card(10, "King"));
+            this.deck.add(new Card(1, "Ace"));
+        }
+
+        this.hand = new ArrayList<Card>();
+    }
+
+    public void shuffleDeck() {
+
+    }
+
+    public HashSet<Card> dealCards(int numberOfCards) {
+    }
 }
--- src/main/java/hwr/oop/Player.java
+++ src/main/java/hwr/oop/Player.java
@@ -1,4 +1,27 @@
 package hwr.oop;
 
 public class Player {
+    private String name;
+    private Set hand;
+    private int stake;
+    private int bankroll;
+    private boolean wantsToHit;
+
+    public Player(String name, int bankroll) {
+        this.hand = new HashSet<Card>();
+        this.name = name;
+        this.bankroll = bankroll;
+    }
+
+    public void setStake(int stake) {
+        this.stake = stake;
+    }
+
+    public void hit(Card card) {
+        hand.add(card);
+    }
+
+    public boolean wantsToHit() {
+        return this.wantsToHit;
+    }
 }
