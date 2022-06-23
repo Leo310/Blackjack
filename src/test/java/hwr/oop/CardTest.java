da8b21531ccc64981bfb985677abb5bc35fc365f
Leo310 <leodev310@gmail.com>
2022-06-23 22:37:20 +0200

    Fix: removed deprecated reference

--- src/test/java/hwr/oop/CardTest.java
+++ src/test/java/hwr/oop/CardTest.java
@@ -8,18 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class CardTest {
 
     @Test
-    void isValidCard() {
-        Card card1 = new Card("King", 10);
-        assertThat(card1.isValidCard()).isTrue();
-        try {
-            Card card2 = new Card("5", 6);
-            card2.isValidCard();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
     void getValue() {
         Card card = new Card("King", 10);
         assertThat(card.getValue()).isEqualTo(10);
@@ -36,14 +24,14 @@ class CardTest {
         Card card1 = new Card("King", 10);
         try {
             card1.setAceValue(11);
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             e.printStackTrace();
         }
         Card card2 = new Card("Ace", 1);
         Card card3 = new Card("Ace", 1);
         try {
             card3.setAceValue(12);
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             e.printStackTrace();
         }
     }
