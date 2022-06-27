1a0c7a4b9b2582673fe307c351d02d2c7970650b
Sebastian Paul <sebastian.paul01@sap.com>
2022-06-27 19:25:29 +0200

    add: Blackjack tests

--- src/test/java/hwr/oop/BlackjackTest.java
+++ src/test/java/hwr/oop/BlackjackTest.java
@@ -3,6 +3,7 @@ package hwr.oop;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BlackjackTest {
@@ -16,14 +17,28 @@ class BlackjackTest {
 
     @Test
     void addPlayer() {
+        assertThat(bj.players.isEmpty()).isTrue();
+        assertThat(bj.addPlayer("Pete", 10)).isFalse();
+        assertThat(bj.addPlayer("Steve", 1000)).isTrue();
+        assertThat(bj.players.isEmpty()).isFalse();
+
     }
 
     @Test
     void removePlayersWithLowBankrolls() {
+        assertThat(bj.players.isEmpty()).isTrue();
+        bj.addPlayer("Pete", 2000);
+        bj.removePlayersWithLowBankrolls();
+        assertThat(bj.players.isEmpty()).isFalse();
+        bj.players.get(0).wins(-1990);
+        bj.removePlayersWithLowBankrolls();
+        assertThat(bj.players.isEmpty()).isTrue();
+
     }
 
     @Test
     void evaluateWinners() {
+
     }
 
     @Test
